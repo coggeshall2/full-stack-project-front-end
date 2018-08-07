14e011e5065c248d363529175271d012f3537144
coggeshall2 <roncogg@hotmail.com>
2018-08-07 15:56:53 -0400

    added new heroku link to config

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://quiet-sands-39548.herokuapp.com/',
+  production: 'https://mighty-caverns-82160.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
